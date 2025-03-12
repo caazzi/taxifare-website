57fd0d1d7952e38bfe30f8746a5fa644b0e331a7
Claudio Azzi <claazzi@gmail.com>
2025-03-12 14:30:38 -0300

    My first website

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ I will need some information to estimate how much your ride will cost.
 ## Please inform the following:
 '''
 
-d = st.date_input("1. date and time", datetime.datetime(2025,03,12))
+d = st.date_input("1. date and time", datetime.datetime(2025,3,12))
 
 '''
 2. pickup location
