621397a59bc3648c4b7314bb51f4c08e3e936592
Claudio Azzi <claazzi@gmail.com>
2025-03-12 11:12:43 -0300

    My first website

---
+++ app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+import requests
+
+'''
+# How much will your taxi cost?
+'''
+
+st.markdown('''
+I will need some information to estimate how much your ride will cost.
+''')
+
+'''
+## Please inform the following:
+
+1. date and time
+'''
+
+
+'''
+2. pickup location
+'''
+
+'''
+3. dropoff location
+'''
+
+'''
+4. passenger count
+'''
+
+
+'''
+## Thanks! Our predicted fare is:
+'''
